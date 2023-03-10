e8aff14ff821ce775cdf5028cf0368f6ba2c6cbe
Ravi Kant <iRvhny@gmail.com>
2023-03-10 16:41:30 +0530

    '20230310_0441PM'

--- DevHunt/settings.py
+++ DevHunt/settings.py
@@ -114,8 +114,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'pyemailing@gmail.com'
-EMAIL_HOST_PASSWORD = 'znjqjdynymlskpqg'
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
+EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASS'
 
 
 # Access using os module:
